21f43ba8bc2c3fce51c713753d16eb29910600ff
Doofus-Mc-Goofus <ericwilliam2904@gmail.com>
2025-06-08 09:59:57 -0400

    dimble

--- Dreamscene/Dreamscene/Properties/AssemblyInfo.cs
+++ Dreamscene/Dreamscene/Properties/AssemblyInfo.cs
@@ -1,18 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
-[assembly: AssemblyTitle("Dynamic Desktop")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dynamic Desktop")]
+[assembly: AssemblyTitle("Wallpaper++")]
+[assembly: AssemblyCompany("DoofusMcGoofus")]
+[assembly: AssemblyProduct("Wallpaper++")]
 [assembly: AssemblyCopyright("GNUv3 License")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
